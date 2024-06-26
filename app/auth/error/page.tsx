79790d208f424ceb50dac8178ff9072c90fc99a6
shivam <thegreatshivam705.gupta@gmail.com>
2024-06-26 10:11:23 +0530

    22

---
+++ app/auth/error/page.tsx
@@ -0,0 +1,8 @@
+import { ErrorCard } from "@/components/ui/auth/error-card";
+
+const AuthErrorPage = () =>{
+    return(
+        <ErrorCard />
+    );
+};
+export default AuthErrorPage;
--- components/ui/auth/login-form.tsx
+++ components/ui/auth/login-form.tsx
@@ -15,10 +15,15 @@ import {
  FormMessage,
 } from "@/components/ui/form"
 import { Button } from "../button";
+import { useSearchParams } from "next/navigation";
 import { FormError } from "@/components/form-error";
 import { FormSuccess } from "@/components/form-success";
 import { login } from "@/actions/login";
 export const LoginForm = () => {
+    const searchParams = useSearchParams();
+    const urlError = searchParams.get("error") === "OAuthAccountNotLinked"
+    ? "Email already in use"
+    :"";
     const [error, setError]=useState<string | undefined >("")
     const [success, setSuccess]=useState<string | undefined >("")
     const [isPending, startTransition ] = useTransition();
@@ -90,7 +95,7 @@ export const LoginForm = () => {
                         )}
                         />
                     </div>
-                    <FormError message={error}/>
+                    <FormError message={error || urlError}/>
                     <FormSuccess message={success}/>
                     <Button
                     disabled={isPending}
