59bec992cdac685a338b841d9c287a4e4b789055
shivam <thegreatshivam705.gupta@gmail.com>
2024-06-26 09:29:37 +0530

    22

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,27 +1,18 @@
 import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
+
 
 const SettingsPage = async () => {
     const session = await auth();
     return(
-        <div className="bg-gray-800">
-            <h1 className="text-blue-100 bg-slate-500 text-4xl">
-              Name: {session?.user?.name}
-              
-            </h1>
-            <h1 className="text-blue-100 bg-slate-500 text-4xl">
-              Email: {session?.user?.email}
-            </h1>
-            <h1 className="text-blue-100 bg-slate-500 text-4xl">
-              Image: {session?.user?.image}
-            </h1>
+        <div >
+            {JSON.stringify(session)}
             <form action={async()=>{
                 "use server"
                 await signOut();
             }}>
-             <Button className="text-3xl">
+             <button >
                 Sign Out
-             </Button>
+             </button>
              
             </form>
         </div>
--- auth.config.ts
+++ auth.config.ts
@@ -3,10 +3,15 @@ import type { NextAuthConfig } from "next-auth"
 import { LoginSchema } from "./schemas";
 import { getUserbyEmail } from "./data/user";
 import bcrypt from "bcryptjs";
+import Google from "next-auth/providers/google"
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
   providers: [
+    Google({
+      clientId : process.env.GOOGLE_CLIENT_ID,
+      clientSecret : process.env.GOOGLE_CLIENT_SECRET,
+    }),
     Credentials({
         async authorize(credentials){
             const validatedFields = LoginSchema.safeParse(credentials);
