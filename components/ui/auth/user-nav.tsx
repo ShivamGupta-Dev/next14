2f37b50891b2407991f11cd30e2120d6a8d6c706
shivam <thegreatshivam705.gupta@gmail.com>
2024-06-29 14:11:26 +0530

    ok

--- components/ui/auth/user-nav.tsx
+++ components/ui/auth/user-nav.tsx
@@ -14,7 +14,7 @@ export const NavUsers = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await fetch('/api/users/providers');
+        const response = await fetch('/api/users');
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
