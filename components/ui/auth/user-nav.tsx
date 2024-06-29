ce29fb0d370e68a89a2682e4efc4f4051ad05868
shivam <thegreatshivam705.gupta@gmail.com>
2024-06-29 13:24:49 +0530

    4

--- components/ui/auth/user-nav.tsx
+++ components/ui/auth/user-nav.tsx
@@ -1,46 +1,49 @@
+// components/NavUsers.tsx
+import { useEffect, useState } from 'react';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import { FaUser } from 'react-icons/fa';
 
-import { UserButton } from "@/components/ui/auth/user-button"
-import { Button } from "@/components/ui/button"
-import  Link  from "next/link"
-import { usePathname } from "next/navigation"
-import { db } from "@/lib/db"
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger
-  } from "@/components/ui/dropdown-menu"
-  import { FaUser } from "react-icons/fa"
-  import{ 
-      Avatar,
-      AvatarImage,
-      AvatarFallback
-  } from "@/components/ui/avatar"
-
-export const NavUsers= async()=>{
-    const users = await db.user.findMany({
-        select: {
-          name: true,
-          image:true,
-        },
-      });
-    return(
-        
-            <ul>
-            {users.map((user, index) => (
-                <li className="bg-secondary flex justify-between items-center p-4 rounded-xl
-                w-[400px] shadow-sm m-5" key={index}><p className="flex gap-x-2">{user.name}</p>
-                
-                <Avatar>
-                    <AvatarImage src={user.image || ""}/>
-                    <AvatarFallback className="bg-sky-200">
-                        <FaUser className="text-white"/>
-                    </AvatarFallback>
-                </Avatar>
-                </li>
-            ))}
-          </ul>
-                
-         
-    )
+interface User {
+  name: string;
+  image: string | null;
 }
+
+export const NavUsers = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('/api/auth/providers');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data: User[] = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <ul>
+      {users.map((user, index) => (
+        <li
+          className="bg-secondary flex justify-between items-center p-4 rounded-xl w-[400px] shadow-sm m-5"
+          key={index}
+        >
+          <p className="flex gap-x-2">{user.name}</p>
+          <Avatar>
+            <AvatarImage src={user.image || ''} />
+            <AvatarFallback className="bg-sky-200">
+              <FaUser className="text-white" />
+            </AvatarFallback>
+          </Avatar>
+        </li>
+      ))}
+    </ul>
+  );
+};
